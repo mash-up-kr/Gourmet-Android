b02e72c51655f3df6ec0783e69967cf97e789e87
len <joenggyu0@gmail.com>
2018-08-25 02:25:15 +0900

    chore: add wishList init resource

---
+++ app/src/main/java/up/mash/gourmet_mash_up/activity/AddWishList_1.java
@@ -0,0 +1,15 @@
+package up.mash.gourmet_mash_up.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import up.mash.gourmet_mash_up.R;
+
+public class AddWishList_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_wish_common);
+    }
+}
