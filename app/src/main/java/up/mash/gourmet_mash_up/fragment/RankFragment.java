b41c259b5121774649c29085294c28796391bb9f
len <joenggyu0@gmail.com>
2018-08-25 06:38:14 +0900

    feat: 랭킹 뷰 완성

--- app/src/main/java/up/mash/gourmet_mash_up/fragment/RankFragment.java
+++ app/src/main/java/up/mash/gourmet_mash_up/fragment/RankFragment.java
@@ -1,6 +1,5 @@
 package up.mash.gourmet_mash_up.fragment;
 
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
@@ -17,61 +16,23 @@ public class RankFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-//    private OnFragmentInteractionListener mListener;
-
     public RankFragment() {
-        // Required empty public constructor
     }
 
     public static RankFragment newInstance(String param1, String param2) {
-        RankFragment fragment = new RankFragment();
-//        Bundle args = new Bundle();
-//        args.putString(ARG_PARAM1, param1);
-//        args.putString(ARG_PARAM2, param2);
-//        fragment.setArguments(args);
-        return fragment;
+        return new RankFragment();
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        if (getArguments() != null) {
-//            mParam1 = getArguments().getString(ARG_PARAM1);
-//            mParam2 = getArguments().getString(ARG_PARAM2);
-//        }
     }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_rank, container, false);
-    }
 
-    public void onButtonPressed(Uri uri) {
-//        if (mListener != null) {
-//            mListener.onFragmentInteraction(uri);
-//        }
-    }
-
-//    @Override
-//    public void onAttach(Context context) {
-//        super.onAttach(context);
-//        if (context instanceof OnFragmentInteractionListener) {
-//            mListener = (OnFragmentInteractionListener) context;
-//        } else {
-//            throw new RuntimeException(context.toString()
-//                    + " must implement OnFragmentInteractionListener");
-//        }
-//    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-//        mListener = null;
-    }
-
-
-    public interface OnFragmentInteractionListener {
-        void onFragmentInteraction(Uri uri);
+        View view = inflater.inflate(R.layout.fragment_rank, container, false);
+        return view;
     }
 }
