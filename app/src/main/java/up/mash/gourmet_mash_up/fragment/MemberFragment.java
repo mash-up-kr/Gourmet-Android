331e704a4027fe66eef33385ad917bffdc5ae75c
len <joenggyu0@gmail.com>
2018-10-02 02:38:40 +0900

    fix: Home 뷰 완성

--- app/src/main/java/up/mash/gourmet_mash_up/fragment/MemberFragment.java
+++ app/src/main/java/up/mash/gourmet_mash_up/fragment/MemberFragment.java
@@ -4,9 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,9 +14,6 @@ import java.util.ArrayList;
 
 import up.mash.gourmet_mash_up.R;
 import up.mash.gourmet_mash_up.adapter.NewsFeedRecyclerViewAdapter;
-import up.mash.gourmet_mash_up.item.FoodItem;
-import up.mash.gourmet_mash_up.item.ItemwithType;
-import up.mash.gourmet_mash_up.item.UserIdItem;
 
 
 public class MemberFragment extends Fragment {
@@ -26,7 +22,7 @@ public class MemberFragment extends Fragment {
 
     NewsFeedRecyclerViewAdapter memberRecyclerViewAdapter;
 
-    public ArrayList<ItemwithType> arrayListOfItems;
+    public ArrayList<Object> arrayListOfItems;
 
     public static MemberFragment newInstance() {
         return new MemberFragment();
@@ -56,36 +52,28 @@ public class MemberFragment extends Fragment {
         memberRecyclerViewAdapter = new NewsFeedRecyclerViewAdapter(arrayListOfItems);
         recyclerView = view.findViewById(R.id.news_feed_recycler_view);
         recyclerView.setAdapter(memberRecyclerViewAdapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setLayoutManager(new GridLayoutManager(getContext(),3));
 
     }
 
     public void initArrayListOfItems() {
         arrayListOfItems = new ArrayList<>();
 
-        for (int i = 0; i < 10; i++) {
-            if (i % 2 == 0) {
-                UserIdItem userIdItem = new UserIdItem();
+        for (int i = 0; i < 31; i++) {
 
-                userIdItem.setUserId("@userId" + (i / 2 + 1));
-
-                arrayListOfItems.add(userIdItem);
-
-                Log.e("create", "userId : " + userIdItem.getUserId());
-            } else {
-                FoodItem foodItem = new FoodItem();
-
-                foodItem.setImageLink("food_ex.png");
-                foodItem.setTradeName("상호명" + (i / 2 + 1));
-                foodItem.setMenuName("메뉴명" + (i / 2 + 1));
-                foodItem.setLocationName("위치명" + (i / 2 + 1));
-                foodItem.setWish((i / 2) % 2 == 0);
-                foodItem.setLikeDislike(!((i / 2) % 2 == 0));
-
-                arrayListOfItems.add(foodItem);
-
-                Log.e("create", "menuName : " + foodItem.getMenuName());
-
+            switch( i%4 ) {
+                case 0:
+                    arrayListOfItems.add(R.drawable.food_ex);
+                    break;
+                case 1:
+                    arrayListOfItems.add(R.drawable.food_ex);
+                    break;
+                case 2:
+                    arrayListOfItems.add(R.drawable.food_ex);
+                    break;
+                case 3:
+                    arrayListOfItems.add(R.drawable.food_ex);
+                    break;
             }
         }
     }
