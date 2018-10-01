b8d35b62baa929a8616c28e2c4648df3de2bfac7
len <joenggyu0@gmail.com>
2018-10-02 03:34:03 +0900

    fix: remote 파일 수정

--- app/src/main/java/up/mash/gourmet_mash_up/data/remote/model/login/RefreshReq.java
+++
@@ -1,9 +0,0 @@
-package up.mash.gourmet_mash_up.data.remote.model.login;
-
-import com.google.gson.annotations.SerializedName;
-
-public class RefreshReq {
-
-    @SerializedName("refreshToken")
-    private String refreshToken;
-}
