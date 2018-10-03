1695620aaa1f08b88fb9c31c0ee6ef839aababcd
len <joenggyu0@gmail.com>
2018-10-03 14:56:05 +0900

    task: glide v4 추가

---
+++ app/src/main/java/up/mash/gourmet_mash_up/app/MyAppGlideModule.java
@@ -0,0 +1,8 @@
+package up.mash.gourmet_mash_up.app;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+@GlideModule
+public class MyAppGlideModule extends AppGlideModule {
+}
